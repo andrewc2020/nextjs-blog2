b432b96eb3163e427a7ef013cc0a9a8fafdddf4c
andrewc2020 <andrewc2020@gmail.com>
2021-10-23 12:23:51 +0100

    added layout

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+     <Head>
+  <title>Create Next App</title>
+  <link rel="icon" href="/favicon.ico" />
+</Head>
       <main>
-        <h1 className="title">
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+  Read{' '}
+  <Link href="/posts/first-post">
+    <a>this page!</a>
+  </Link>
+</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -45,6 +48,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
+          
         </div>
       </main>
 
