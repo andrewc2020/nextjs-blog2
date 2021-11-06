1b068ab25cf87d496279a1fc682966971c671bb9
andrewc2020 <andrewc2020@gmail.com>
2021-11-06 12:05:18 +0000

    updated profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,37 @@
 import Layout, { siteTitle } from '../components/layout'
 import Mailto from '../components/mailto'
+
+
+
+
+
 export default function Profile() {
 
     
-    return <Layout Profile> <h1>Mon profil</h1><h2>Mon expérience</h2><Mailto email="someone@somewhere.com" subject="Salut" body="Salut Glyn,">Contactez moi</Mailto> </Layout>
-  }
+    return 
+  <Layout Profile> <h1>Mon profil</h1><article><p>Double nationalité Britannique et Française, domicilié en Bretagne au Conquet.</p>
+    <p>Co fondateur de ENGLISH APART à Brest en 1985, Méthode pédagogique de l’enseignement
+    de la langue anglaise basée sur des mises en situation et adaptée aux adultes dans le cadre de
+    la formation continue.
+    Co dirigeant de l’établissement ENGLISH APART, formateur en langue anglaise et coach
+    auprès des professionnels SCIENTIFIQUES, COMMERCIAUX et TECHNIQUES.
+
+    Expérience dans des domaines pluri disciplinaires :
+    Contrôle aérien, Groupes électrogènes, Energies renouvelables, Domaine médical :
+    immunologie, cardiologie, ophtalmologie, Recherche scientifique : données scientifiques,
+    biologie, géologie, Carénage de bateaux, Industrie automobile : générateurs de gaz (inflators)
+    ...</p>
+
+    <h2>Depuis 2021</h2>
+    <p>Auto Entrepreneur
+
+    Facilitateur et Consultant en langue anglaise
+
+    Accompagnateur de projets professionnels, préparation de congrès internationaux en langue
+    anglaise
+    Mises en forme et corrections de publications scientifiques, commerciales et techniques.
+    Accueil des Collaborateurs et Clients en Anglais (animation autour de repas d’affaires)
+    Cours d’anglais en visio ou en présentiel.
+    Stage en HOMESTAY au Conquet.</p></article><Mailto email="someone@somewhere.com" subject="Salut" body="Salut Glyn,">Contactez moi</Mailto>
+  </Layout>
+}
---
+++ profile/profile.md
@@ -0,0 +1,30 @@
+---
+title: 'Mon Profile'
+date: '2020-04-02'
+---
+
+**Mon profil** :
+Double nationalité Britannique et Française, domicilié en Bretagne au Conquet.
+Co fondateur de ENGLISH APART à Brest en 1985, Méthode pédagogique de l’enseignement
+de la langue anglaise basée sur des mises en situation et adaptée aux adultes dans le cadre de
+la formation continue.
+Co dirigeant de l’établissement ENGLISH APART, formateur en langue anglaise et coach
+auprès des professionnels SCIENTIFIQUES, COMMERCIAUX et TECHNIQUES.
+
+Expérience dans des domaines pluri disciplinaires :
+Contrôle aérien, Groupes électrogènes, Energies renouvelables, Domaine médical :
+immunologie, cardiologie, ophtalmologie, Recherche scientifique : données scientifiques,
+biologie, géologie, Carénage de bateaux, Industrie automobile : générateurs de gaz (inflators)
+...
+
+**Depuis 2021**
+Auto Entrepreneur
+
+Facilitateur et Consultant en langue anglaise
+
+Accompagnateur de projets professionnels, préparation de congrès internationaux en langue
+anglaise
+Mises en forme et corrections de publications scientifiques, commerciales et techniques.
+Accueil des Collaborateurs et Clients en Anglais (animation autour de repas d’affaires)
+Cours d’anglais en visio ou en présentiel.
+Stage en **HOMESTAY** au Conquet.
