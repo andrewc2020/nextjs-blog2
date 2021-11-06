da068df83728f2c75147a9c0e9537dbf39d6ff79
andrewc2020 <andrewc2020@gmail.com>
2021-11-06 12:58:43 +0000

    added retournez

--- components/layout.js
+++ components/layout.js
@@ -72,7 +72,7 @@ export default function Layout({ children, home }) {
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
-            <a>← Back to home</a>
+            <a>← Retournez</a>
           </Link>
         </div>
       )}
