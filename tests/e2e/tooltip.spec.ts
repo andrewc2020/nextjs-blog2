b727042aba65c34c1d1438497e72dc6c977cdb6a
andrewc2020 <andrewc2020@gmail.com>
2021-12-08 17:35:21 +0000

    attempting to trap hover

--- tests/e2e/tooltip.spec.ts
+++ tests/e2e/tooltip.spec.ts
@@ -2,11 +2,12 @@ import {test, expect} from '@playwright/test'
 
 test('tooltip should be rendered as a span with existing text', async({page})=>{
     await page.goto('/')
-    const location = page.locator('text=Tarifs >> nth=0')
+    const location = page.locator("text=Tarifs6 >> a")
     await location.hover().then(() =>{
         
-            expect(page.locator('.tooltip')).toBeVisible()
+        
+            expect(page.locator('.tooltip')).toHaveText("tooltip")
     
         
-    })
-})
+    
+})})
