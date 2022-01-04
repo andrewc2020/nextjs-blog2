dccc0236da59d02022ac524117e66ccb39faa1d4
andrewc2020 <andrewc2020@gmail.com>
2022-01-04 22:46:18 +0000

    added video

--- tests/unit/video.spec.tsx
+++ tests/unit/video.spec.tsx
@@ -4,6 +4,6 @@ import VideoPlayer from '../../components/videoplayer'
 it('should render a video tag', async ()=>{
     render(< VideoPlayer />)
     screen.debug()
-    expect(await screen.queryByRole('source')).toBeVisible()
+    expect(await screen.getByTestId('custom-element')).toBeTruthy()
 
 })
