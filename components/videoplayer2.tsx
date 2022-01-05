58f0472a572bf5521621c5da7899905c1a820ac2
andrewc2020 <andrewc2020@gmail.com>
2022-01-05 23:09:14 +0000

    relative div around video

--- components/videoplayer2.tsx
+++ components/videoplayer2.tsx
@@ -4,8 +4,8 @@ export default function VideoPlayer() {
 
     return(
         
-        <video width="500" data-testid="custom-element" controls>
-      <source src="https://ik.imagekit.io/eudt3rlpetf/video/MBVV8571_converted_ia2BjnyJx.mp4?tr=w-500" type="video/mp4"/>
+        <video width="100%" data-testid="custom-element" controls>
+      <source src="https://ik.imagekit.io/eudt3rlpetf/video/MBVV8571_converted_ia2BjnyJx.mp4" type="video/mp4"/>
      </video>
        
     )
