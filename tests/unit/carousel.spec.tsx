ce84183e02337bd98bd7d848c5f58b891d093d04
andrewc2020 <andrewc2020@gmail.com>
2022-01-08 16:26:07 +0000

    removed carousel test

--- tests/unit/carousel.spec.tsx
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-import { screen, render } from '@testing-library/react'
-import Carousel from '../../components/carousel'
-
-it('should render a carousel',()=>{
-    render(<Carousel />)
-})
