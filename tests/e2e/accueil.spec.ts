7cb54bb5e21d20b72f6d33ed7d163a01a13e48a6
andrewc2020 <andrewc2020@gmail.com>
2023-02-23 12:30:14 +0000

    e2e tests passed

--- tests/e2e/accueil.spec.ts
+++ tests/e2e/accueil.spec.ts
@@ -5,7 +5,7 @@ test('a link within a post successfully delivers the page', async({page}) =>{
 
     await page.goto('/posts/accueildesclients')
     // Find an element with the text 'Glyn' and click on it
-    await page.click('text=exemples')
+    await page.click('text=moments de convivialité')
 
     await expect(page).toHaveURL('/posts2/prise')
 })
